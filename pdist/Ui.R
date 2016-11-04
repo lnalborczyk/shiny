f9ae0202f8f7ba0d7a02ccb0fde39420148d8a0a
lnalborczyk <ladislas.nalborczyk@gmail.com>
2016-11-04 13:40:35 +0100

    add p distribution shiny

---
+++ pdist/Ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+fluidPage(
+  
+  titlePanel(""),
+
+  sidebarLayout(
+    position = "right",
+    sidebarPanel(
+      h4("effect size (d) and sample size (n)"),
+      sliderInput("d", "d", min = 0, max = 2, value = 0.5, step = 0.01),
+      sliderInput("n", "n", min = 1, max = 100, value = 20, step = 1)
+    ),
+
+    mainPanel(
+      plotOutput("Plot"), withMathJax(helpText('distribution of 1000 p-values for an independant-sample t-test, as a function of effect size (Cohen\'s d) and sample size'))
+    )
+
+  )
+)
---
+++ pdist/server.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+function(input, output, session) {
+  
+  output$Plot <- renderPlot({
+    
+      options(scipen = 999) # disable scientific notation for numbers
+      
+      cohensd <- input$d  # set true effect size
+      n <- input$n # sample size in each group
+      
+      nSims <- 1000 # number of simulated experiments
+      p <- numeric(nSims) # set up empty variable to store all simulated p-values
+      
+      for(i in 1:nSims){
+          
+          x <- rnorm(n = n, mean = 0, sd = 1) # produce N simulated participants
+          y <- rnorm(n = n, mean = 0 + cohensd, sd = 1) # produce N simulated participants
+          z <- t.test(x,y) # perform a t-test for independant samples
+          
+          p[i] <- z$p.value # extract p-value
+          
+      }
+      
+      empirical_power <- (sum(p < 0.05) / nSims)
+      
+      if(cohensd==0){empirical_power <- 0} # if H0, then power = 0
+      
+      hist(p, breaks = 20, ylab = "number of p-values", border = FALSE, 
+          main = paste("p-value distribution with", 
+              round(empirical_power * 100, digits = 2), "% power"), 
+          xlim = c(0, 1), ylim = c(0, nSims), col = "steelblue")
+      
+      abline(h = nSims / 20, lty = 2)
+
+  })
+  
+}
