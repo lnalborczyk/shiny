abf8744fd3744d33f57904b44796adb18b8e5f4f
lnalborczyk <ladislas.nalborczyk@gmail.com>
2018-02-24 18:15:00 +0100

    updates on pdist

--- pdist/Ui.R
+++ pdist/Ui.R
@@ -1,20 +1,30 @@
+library(tidyverse)
 library(shiny)
 
 fluidPage(
   
-  titlePanel(""),
-
-  sidebarLayout(
-    position = "right",
-    sidebarPanel(
-      h4("effect size (d) and sample size (n)"),
-      sliderInput("d", "d", min = 0, max = 2, value = 0.5, step = 0.01),
-      sliderInput("n", "n", min = 1, max = 100, value = 20, step = 1)
-    ),
-
-    mainPanel(
-      plotOutput("Plot"), withMathJax(helpText('distribution of 1000 p-values for an independant-sample t-test, as a function of effect size (Cohen\'s d) and sample size'))
+    titlePanel(""),
+    
+    sidebarLayout(
+        
+        position = "right",
+        
+        sidebarPanel(
+            h4("Effect size (d), sample size (n), and alpha level."),
+            sliderInput("d", "d", min = 0, max = 2, value = 0.5, step = 0.01),
+            sliderInput("n", "n", min = 1, max = 100, value = 20, step = 1),
+            sliderInput("alpha", "alpha", min = 0, max = 1, value = 0.05, step = 0.01)
+            ),
+    
+        mainPanel(
+            plotOutput("Plot"),
+            withMathJax(
+                helpText(
+                    "Distribution of p-values for an independant-samples t-test,
+                    as a function of effect size (Cohen\'s d) and sample size."
+                    )
+                )
+            )
+      
+      )
     )
-
-  )
-)
--- pdist/server.R
+++ pdist/server.R
@@ -4,35 +4,39 @@ function(input, output, session) {
   
   output$Plot <- renderPlot({
     
-      options(scipen = 999) # disable scientific notation for numbers
-      
       cohensd <- input$d  # set true effect size
       n <- input$n # sample size in each group
+      alpha <- input$alpha
       
-      nSims <- 1000 # number of simulated experiments
+      nSims <- 1e4 # number of simulated experiments
       p <- numeric(nSims) # set up empty variable to store all simulated p-values
       
-      for(i in 1:nSims){
+      for (i in 1:nSims) {
           
           x <- rnorm(n = n, mean = 0, sd = 1) # produce N simulated participants
           y <- rnorm(n = n, mean = 0 + cohensd, sd = 1) # produce N simulated participants
-          z <- t.test(x,y) # perform a t-test for independant samples
+          z <- t.test(x, y) # perform a t-test for independant samples
           
           p[i] <- z$p.value # extract p-value
           
       }
       
-      empirical_power <- (sum(p < 0.05) / nSims)
+      empirical_power <- (sum(p < alpha) / nSims)
       
-      if(cohensd==0){empirical_power <- 0} # if H0, then power = 0
+      if (cohensd == 0) {empirical_power <- 0} # if H0, then power = 0
       
-      hist(p, breaks = 20, ylab = "number of p-values", border = FALSE, 
-          main = paste("p-value distribution with", 
-              round(empirical_power * 100, digits = 2), "% power"), 
-          xlim = c(0, 1), ylim = c(0, nSims), col = "steelblue")
+      data.frame(p = p) %>%
+          ggplot(aes(x = p) ) +
+          stat_density(fill = "grey60") +
+          geom_vline(xintercept = alpha, size = 0.7, linetype = 3) +
+          labs(x = "p-value", y = "") +
+          theme_bw(base_size = 14, base_family = "Verdana") +
+          ggtitle(
+              paste0(
+                  "p-value distribution for d = ", cohensd, " and n = ", n,
+                  "\npower = ", round(empirical_power, digits = 2),
+                  " (at alpha = ", alpha, ")") )
       
-      abline(h = nSims / 20, lty = 2)
-
   })
   
 }
